28497fb1457315404c005b3c9bdfc0450d1b4769
Александр <60600910+AleksandrKuprienko@users.noreply.github.com>
2020-09-11 01:16:03 +0300

    pretty code

--- lib/random_color.dart
+++ lib/random_color.dart
@@ -4,5 +4,5 @@ import 'package:flutter/material.dart';
 Random random = Random();
 
 class RandomColor{
-  Color generationColor() =>Color(0xFF000000 + random.nextInt(0x00FFFFFF));
+  Color generationColor() => Color(0xFF000000 + random.nextInt(0x00FFFFFF));
 }
